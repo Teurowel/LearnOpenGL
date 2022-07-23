961278f45a9b73f0a7949502ad1b58a34aa8eff6
장두성 <jds2613@nimbleneuron.com>
2022-07-23 12:14:16 +0900

    Textures : Added texture

--- LearnOpenGL/LearnOpenGL/Source.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-int CreateGLFWWindow(bool& retflag);
--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -1,8 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include "Source.h"
 #include "Shader.h"
+#include "stb_image.h"
 
 GLFWwindow* window = nullptr;
 unsigned int vertexShaderID = 0;
@@ -14,6 +14,15 @@ unsigned int fragmentShaderID2 = 0;
 unsigned int triangleVAO = 0;
 unsigned int triangle2VAO = 0;
 
+unsigned int rectangleVAO = 0;
+
+unsigned int VBO = 0;
+
+unsigned int EBO = 0;
+
+unsigned int texture1 = 0;
+unsigned int texture2 = 0;
+
 Shader shader;
 
 bool isWireFrameMode = false;
@@ -24,6 +33,12 @@ float triangleVertices[] = {
 	 -0.5f,  0.5f, 0.0f,	0.0f, 0.0f, 1.0f
 };
 
+float triangleTexCoords[] = {
+	0.0f, 0.0f,
+	1.0f, 0.0f,
+	0.5f, 1.0f
+};
+
 float triangleVertices2[] = {
 	 0.0f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,
 	 1.0f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,
@@ -31,10 +46,11 @@ float triangleVertices2[] = {
 };
 
 float rectangleVertices[] = {
-	 0.5f,  0.5f, 0.0f,  // top right
-	 0.5f, -0.5f, 0.0f,  // bottom right
-	-0.5f, -0.5f, 0.0f,  // bottom left
-	-0.5f,  0.5f, 0.0f   // top left 
+	// positions          // colors           // texture coords
+	0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+	0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 };
 unsigned int rectangleIndices[] = {  // note that we start from 0!
 	0, 1, 3,   // first triangle
@@ -121,16 +137,20 @@ void ClearBuffer()
 
 void RenderLoop(GLFWwindow* window)
 {
+	shader.Use(); // don't forget to activate/use the shader before setting uniforms!
+
+	//Set sampler's texture unit
+	shader.SetInt("texture1", 0);
+	shader.SetInt("texture2", 1);
+
 	while (glfwWindowShouldClose(window) == false)
 	{
 		ProcessInput(window);
 
 		ClearBuffer();
 
-		shader.Use();
-		shader.SetFloat("horizontalOffset", 0.2f);
-		glBindVertexArray(triangleVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindVertexArray(triangleVAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//float timeValue = glfwGetTime();
 		//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
@@ -139,18 +159,29 @@ void RenderLoop(GLFWwindow* window)
 		//Note that finding the uniform location does not require you to use the shader program first, but updating a uniform does require you to first use the program(by calling glUseProgram), because it sets the uniform on the currently active shader program.
 		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		
-		glBindVertexArray(triangle2VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		glfwPollEvents(); //checks if any events are triggered (like keyboard input or mouse movement events), updates the window state, and calls the corresponding functions (which we can register via callback methods)
+		shader.Use();
+		glBindVertexArray(rectangleVAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		glfwSwapBuffers(window); //swap the color buffer (a large 2D buffer that contains color values for each pixel in GLFW's window) that is used to render to during this render iteration and show it as output to the screen.
-		
+		glfwPollEvents(); //checks if any events are triggered (like keyboard input or mouse movement events), updates the window state, and calls the corresponding functions (which we can register via callback methods)		
 	}
 }
 
 void Clear()
 {
+	glDeleteVertexArrays(1, &rectangleVAO);
+	glDeleteVertexArrays(1, &triangleVAO);
+	glDeleteVertexArrays(1, &triangle2VAO);
+
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
 	glfwTerminate();
 }
 
@@ -165,7 +196,6 @@ void InitVertexArrayObject(unsigned int& VAO)
 
 void InitVertexBuffer(int verticesSize, const void* vertices)
 {
-	unsigned int VBO = 0;
 	glGenBuffers(1, &VBO); //Generate Buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO); //bind buffer at GL_ARRAY_BUFFER 
 	glBufferData(GL_ARRAY_BUFFER, verticesSize, vertices, GL_STATIC_DRAW); //set data to GL_ARRAY_BUFFER, stored data in memory on graphics card
@@ -176,22 +206,75 @@ void InitVertexAttributes()
 	//Each vertex attribute takes its data from memory managed by a VBO and which VBO it takes its data from (you can have multiple VBOs) is determined by the VBO currently bound to GL_ARRAY_BUFFER when calling glVertexAttribPointer
 	//we specified how OpenGL should interpret the vertex data
 
+	////Position attribute
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); //location in Vertexshader, size of vertex attribute(vec3), normalize data, stride(space between data), offset where position data begin
+	//glEnableVertexAttribArray(0);
+
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	//glEnableVertexAttribArray(1);
+
 	//Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); //location in Vertexshader, size of vertex attribute(vec3), normalize data, stride(space between data), offset where position data begin
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); //location in Vertexshader, size of vertex attribute(vec3), normalize data, stride(space between data), offset where position data begin
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 }
 
 void InitElementBufferObject()
 {
 	//An EBO is a buffer, just like a vertex buffer object, that stores indices that OpenGL uses to decide what vertices to draw. This so called indexed drawing
-	unsigned int EBO = 0;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rectangleIndices), rectangleIndices, GL_STATIC_DRAW);
+}
+
+void InitTexture(const char* texturePath, unsigned int& textureID, GLenum sourceImageFormat)
+{
+	//Create and bind Texture
+	//unsigned int textureID = 0;
+	glGenTextures(1, &textureID);
+
+	//glActiveTexture(GL_TEXTURE0);// activate the texture unit first before binding texture
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	//Texture Wrapping, s, t (and r if you're using 3D textures) equivalent to x,y,z
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	//Border color
+	/*float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);*/
+
+	//Texture Filtering
+	//A common mistake is to set one of the mipmap filtering options as the magnification filter. This doesn't have any effect since mipmaps are primarily used for when textures get downscaled
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	//Load Texture
+	int width = 0;
+	int height = 0;
+	int nrChannels = 0;
+	stbi_set_flip_vertically_on_load(true); //OpenGL expects the 0.0 coordinate on the y-axis to be on the bottom side of the image, but images usually have 0.0 at the top of the y-axis
+	unsigned char* data = stbi_load(texturePath, &width, &height, &nrChannels, 0);
+
+	//Generate texture
+	if (data != nullptr)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, sourceImageFormat, GL_UNSIGNED_BYTE, data); //Target for generating texture, mipmap level, format to store, width, height, legacy, source image's format, source image's data type
+		glGenerateMipmap(GL_TEXTURE_2D); //generate mipmap
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data); //After we're done generating the texture and its corresponding mipmaps, it is good practice to free the image memory
 }
 
 
@@ -218,25 +301,27 @@ int main()
 
 
 	// 1. bind Vertex Array Object
-	InitVertexArrayObject(triangleVAO);
+	InitVertexArrayObject(rectangleVAO);
 
 	// 2. copy our vertices array in a buffer for OpenGL to use
-	InitVertexBuffer(sizeof(triangleVertices), triangleVertices);
+	InitVertexBuffer(sizeof(rectangleVertices), rectangleVertices);
 
 	// 3. copy our index array in a element buffer for OpenGL to use
-	//InitElementBufferObject();
+	InitElementBufferObject();
 
 	// 4. then set the vertex attributes pointers
 	InitVertexAttributes();
 
 
-	InitVertexArrayObject(triangle2VAO);
+	/*InitVertexArrayObject(triangle2VAO);
 	InitVertexBuffer(sizeof(triangleVertices2), triangleVertices2);
-	InitVertexAttributes();
+	InitVertexAttributes();*/
 
 	
-	shader.InitShader("shader.vs", "shader.fs");
+	shader.InitShader("Shader/shader.vs", "Shader/shader.fs");
 	
+	InitTexture("Texture/container.jpg", texture1, GL_RGB);
+	InitTexture("Texture/awesomeface.png", texture2, GL_RGBA);
 
 	// 5. now draw the object 
 	RenderLoop(window);
@@ -244,4 +329,5 @@ int main()
 	Clear();
 	return 0;
 }
+
 
