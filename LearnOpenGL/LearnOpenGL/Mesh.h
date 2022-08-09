31367be31252a532d770a511ac719ce40ef43a57
장두성 <jds2613@nimbleneuron.com>
2022-08-09 10:44:44 +0900

    Added Assimp(Asset importer) Added Mesh class

---
+++ LearnOpenGL/LearnOpenGL/Mesh.h
@@ -0,0 +1,43 @@
+﻿#ifndef MESH_H
+#define MESH_H
+
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+
+class Shader;
+
+struct Vertex
+{
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texCoords;
+};
+
+struct Texture
+{
+    unsigned int id;
+    std::string type;
+};
+
+class Mesh
+{
+public:
+    //mesh data
+    std::vector<Vertex>         vertices;
+    std::vector<unsigned int>   indices;
+    std::vector<Texture>        textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(Shader& shader);
+
+private :
+    //render data
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int EBO;
+
+    void SetupMesh();
+};
+
+#endif
